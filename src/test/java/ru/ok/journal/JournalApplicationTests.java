883f0e4c62eece6d7348eb0ff3486b13a40e0990
shesternev <shesternevga@gmail.com>
2020-02-17 18:53:20 +0300

    added spring skeleton

---
+++ src/test/java/ru/ok/journal/JournalApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ok.journal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
