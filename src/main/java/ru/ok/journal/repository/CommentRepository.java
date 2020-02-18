341ef43dd89a0161a1a956973d390f561dd00912
shesternev <shesternevga@gmail.com>
2020-02-18 13:46:06 +0300

    update CommentRepository: added findByPost method

--- src/main/java/ru/ok/journal/repository/CommentRepository.java
+++ src/main/java/ru/ok/journal/repository/CommentRepository.java
@@ -2,10 +2,12 @@ package ru.ok.journal.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.ok.journal.model.Comment;
+import ru.ok.journal.model.Post;
 import ru.ok.journal.model.User;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findAllByAuthor(User user);
+    List<Comment> findAllByPost(Post post);
 }
