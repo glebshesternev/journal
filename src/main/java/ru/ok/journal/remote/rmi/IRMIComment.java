0425807094d9d0ebaf63459d72d42dc11c3b4f65
NEE_TT <Nshmendyuk.ru@gmail.com>
2020-07-06 20:24:27 +0300

    add delete comments and rmi updated

--- src/main/java/ru/ok/journal/remote/rmi/IRMIComment.java
+++ src/main/java/ru/ok/journal/remote/rmi/IRMIComment.java
@@ -1,12 +1,10 @@
 package ru.ok.journal.remote.rmi;
 
-
-
 import ru.ok.journal.remote.entity.RMIComment;
 
 import java.util.List;
 
 public interface IRMIComment  {
     public List<RMIComment> getByPost(List<Pair> postIds);
-
+    public void delete(Long commentId);
 }
